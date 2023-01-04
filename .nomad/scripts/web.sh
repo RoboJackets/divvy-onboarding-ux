cd99c914dce36c6d5c465622bf2813e1737fade6
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-01-04 17:47:31 -0500

    fix wsgi app location

--- .nomad/scripts/web.sh
+++ .nomad/scripts/web.sh
@@ -1,1 +1,1 @@
-uwsgi --uwsgi-socket /var/opt/nomad/run/${NOMAD_JOB_NAME}-${NOMAD_ALLOC_ID}.sock --http-socket 0.0.0.0:${NOMAD_PORT_http} --chdir=/app/ --module=divvy_onboarding_ux.app
+uwsgi --uwsgi-socket /var/opt/nomad/run/${NOMAD_JOB_NAME}-${NOMAD_ALLOC_ID}.sock --http-socket 0.0.0.0:${NOMAD_PORT_http} --chdir=/app/ --module=divvy_onboarding_ux:app
