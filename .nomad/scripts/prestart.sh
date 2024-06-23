d7be92a0d965830a5641499129e5ecf62367a137
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-06-23 16:45:06 -0400

    Remove robots.txt

--- .nomad/scripts/prestart.sh
+++ .nomad/scripts/prestart.sh
@@ -1,4 +1,3 @@
 mkdir --parents /assets/${NOMAD_JOB_NAME}/static/
 cp --recursive --verbose static/* /assets/${NOMAD_JOB_NAME}/static/
-cp --verbose static/robots.txt /assets/${NOMAD_JOB_NAME}/
 cp --verbose static/favicon.ico /assets/${NOMAD_JOB_NAME}/
